f619450bca20389543d9b3a5fa09904c90eded16
AnonHexo <61375258+AnonHexo@users.noreply.github.com>
2020-05-22 10:19:52 +0200

    Update images.py

--- userbot/plugins/images.py
+++ userbot/plugins/images.py
@@ -9,7 +9,7 @@ from re import findall
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("img ?(.*)"))
+@borg.on(admin_cmd("img ?(query*)"))
 async def img_sampler(event):
     await event.edit("Processing...")
     query = event.pattern_match.group(1)
