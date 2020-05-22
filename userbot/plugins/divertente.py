2442629b77c05a9770ea100350e8e5e94686a912
AnonHexo <61375258+AnonHexo@users.noreply.github.com>
2020-05-22 09:45:45 +0200

    Update divertente.py

--- userbot/plugins/divertente.py
+++ userbot/plugins/divertente.py
@@ -10,29 +10,7 @@ from userbot.utils import admin_cmd
 import asyncio
 
 
-@borg.on(admin_cmd(pattern="puta"))
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    animation_interval = 0.8
-    animation_ttl = range(0, 16)
-    input_str = event.pattern_match.group(1)
-    if input_str == "puta":
-        await event.edit(input_str)
-        animation_chars = [
-            "Shh",
-            "STAI",
-            "SEDUTA",
-            "PUTA🤫🤫"
-        ]
-        for i in animation_ttl:
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 4])
-
-
-
-@borg.on(admin_cmd(pattern="fottiti"))
+@borg.on(admin_cmd(pattern="fuck"))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
@@ -47,7 +25,7 @@ async def _(event):
 
     input_str = event.pattern_match.group(1)
 
-    if input_str == "fottiti":
+    if input_str == "fuck":
 
         await event.edit(input_str)
 
