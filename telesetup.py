6546167a33344eb6266060d0e896572162f5920b
AnonHexo <61375258+AnonHexo@users.noreply.github.com>
2020-05-22 16:22:10 +0200

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -11,7 +11,7 @@ print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details""")
-APP_ID = input("Enter APP ID here: "))
+APP_ID = input("Enter APP ID here: ")
 API_HASH = input("Enter API HASH here: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
