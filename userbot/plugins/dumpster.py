7d9948bafa62554d14c508027bb1dd35f99c3f04
AnonHexo <61375258+AnonHexo@users.noreply.github.com>
2020-05-22 09:52:04 +0200

    Update dumpster.py

--- userbot/plugins/dumpster.py
+++ userbot/plugins/dumpster.py
@@ -3,7 +3,7 @@ import asyncio
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("dump ?(.*)"))
+@borg.on(admin_cmd("magna ?(.*)"))
 async def _(message):
     try:
         obj = message.pattern_match.group(1)
